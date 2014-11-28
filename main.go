fe13deb936d8ee51dae31d78f6b076e280ca00b0
Johannes 'fish' Ziemke <github@freigeist.org>
2014-11-28 14:26:40 +0100

    Add pprof debugging + flag

--- main.go
+++ main.go
@@ -3,15 +3,26 @@ package main
 import (
 	"flag"
 	"log"
+	"net/http"
+	_ "net/http/pprof"
 
 	"github.com/docker-infra/bytes-piper/pipeline"
 )
 
 var (
-	config = flag.String("c", "", "path to config")
+	config        = flag.String("c", "", "path to config")
+	debugEndpoint = flag.String("d", "", "enable pprof debugging endpoint on given host:port")
 )
 
 func main() {
+	var listenErr chan error
+	if *debugEndpoint != "" {
+		go func() {
+			listenErr <- http.ListenAndServe(*debugEndpoint, nil)
+
+		}()
+	}
+
 	flag.Parse()
 	if *config == "" {
 		log.Fatal("No config provided")
@@ -23,5 +34,10 @@ func main() {
 
 	if err := pipe.Run(); err != nil {
 		log.Fatal(err)
+	}
+
+	if *debugEndpoint != "" {
+		log.Print("Debugging enabled, keep listening for debugging")
+		log.Print(<-listenErr)
 	}
 }
